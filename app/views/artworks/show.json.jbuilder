cd113ab4e5df264715dee8f71c2a8477bdf66a19
Robin Janse <r.janse@bluetools.nl>
2013-04-22 19:56:15 +0200

    Scaffolded the Artwork resource

---
+++ app/views/artworks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @artwork, :title, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Exhibits::Application.routes.draw do
+  resources :artworks
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'artworks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
