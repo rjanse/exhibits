245fe04a5a2b03b8a479d1324852248c2bf208e8
Robin Janse <r.janse@bluetools.nl>
2013-04-22 21:57:42 +0200

    Scenario: update a registered artwork

--- spec/features/managing_artworks.feature
+++ spec/features/managing_artworks.feature
@@ -1,7 +1,14 @@
 Feature: Managing artworks
   Scenario: register an artwork
     Given   I have 0 artworks
-    When    I vist the new artwork path
-    And     I fill in the form
-    And     I submit the form
+    When    I visit the new artwork path
+    And     I fill in the form with Mask
+    And     I submit the new form
     Then    I have 1 artwork
+  
+  Scenario: update a registered artwork
+    Given   I created an artwork with the title Flower
+    When    I visit the edit artwork path
+    And     I fill in the form with Power
+    And     I submit the edit form
+    Then    I should have a updated artwork with the title Power
--- spec/steps/artwork_steps.rb
+++ spec/steps/artwork_steps.rb
@@ -1,17 +1,39 @@
 module ArtworkSteps
+  
+  def artwork
+    @artwork ||= Artwork.create(title: "Flower")
+  end
+  
   step "I have :amount artwork(s)" do |amount|
     Artwork.count.should eq amount.to_i
   end
 
-  step "I vist the new artwork path" do
+  step "I visit the new artwork path" do
     visit new_artwork_path
   end
-  
-  step "I fill in the form" do
-    fill_in('Title', :with => 'Mask')
+
+  step "I visit the edit artwork path" do
+    visit edit_artwork_path(artwork)
   end
   
-  step "I submit the form" do
+  step "I fill in the form with :title" do |title|
+    fill_in('Title', with: title)
+  end
+  
+  step "I submit the new form" do
     click_button('Create Artwork')
   end
+
+  step "I submit the edit form" do
+    click_button('Update Artwork')
+  end
+
+  step "I created an artwork with the title Flower" do
+    artwork
+  end
+
+  step "I should have a updated artwork with the title Power" do
+    Artwork.exists?(title: "Power").should be_true
+  end
+
 end
