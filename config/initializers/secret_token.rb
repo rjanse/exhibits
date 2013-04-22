91e4ca70468e2b8da1ab454c4eb2cc023a18be03
Robin Janse <r.janse@bluetools.nl>
2013-04-22 16:26:52 +0200

    New project setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exhibits::Application.config.secret_key_base = '4284194c7e4ca49a5f91dada27e3ba0a9b410069868422e628c2c60e072c5d4ac3b39fbc631a3ce973af55c55615a91464eed9bf037cfdc401ab9048b7423529'
