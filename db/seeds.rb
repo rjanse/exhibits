f862499b1fed9cb70be09b5d9d6a746e63d80e28
Robin Janse <r.janse@bluetools.nl>
2013-04-24 20:55:52 +0200

    better seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
-['Aquis submersus', 'Chapeau de Paille', 'Configuration', 'Cut with the Kitchen Knife', 'Dadaville', 'Feathers', 'Forest', 'Legend'].each do |title|
+artworks  = ['Aquis submersus', 'Chapeau de Paille', 'Configuration', 'Cut with the Kitchen Knife', 'Dadaville', 'Feathers', 'Forest', 'Legend']
+
+artworks.each do |title|
   Artwork.create(title: title)
+end
+
+# Past Exhibitions
+5.times do |index|
+  gallery = "Gallery ##{index + 1}"
+  start_month = (index + 2).months
+  end_month = (index + 1).months
+  Exhibition.create(gallery: gallery, start_date: start_month.ago.beginning_of_month, end_date: end_month.ago.end_of_month)
+  Exhibition.create(gallery: gallery, start_date: start_month.from_now.beginning_of_month, end_date: end_month.from_now.end_of_month)
 end
